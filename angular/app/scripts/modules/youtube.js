13aa8e3f17b763a1f05a9b89b0f0f94477697088
Oleg Ishenko <oleg.ishenko@solvingmagento.com>
2013-11-23 20:46:13 +0100

    Moved the directives template

--- angular/app/scripts/modules/youtube.js
+++ angular/app/scripts/modules/youtube.js
@@ -1,14 +1,11 @@
 'use strict';
 angular.module('ytdirectives', []);
-angular.module('ytdirectives')
-.directive('youtube', function() {
+angular.module('ytdirectives').directive('youtube', function() {
   return {
     restrict: 'E',
-    templateUrl: 'views/directives/youtube.html',
+    templateUrl: 'views/youtube.html',
     link: function (scope, element, attrs) {
       element[0].innerHTML = element[0].innerHTML.replace('%url%', 'http://www.youtube.com/embed/' + attrs.code);
     }
   };
 });
-
-
