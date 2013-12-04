70ea68cedb4eb1220edbaf4a9b6b73f5e92be987
Oleg Ishenko <oleg.ishenko@solvingmagento.com>
2013-12-04 23:13:50 +0100

    Added first player controls

--- angular/app/scripts/controllers/main.js
+++ angular/app/scripts/controllers/main.js
@@ -1,8 +1,44 @@
 'use strict';
+var YT, callPlayer;
 
 angular.module('angularApp').controller(
   'MainCtrl',
   ['$scope', 'Player', function ($scope, Player) {
-    $scope.player = Player;
-    $scope.player();
+
+    var playerElementId = 'player';
+
+    $scope.loadVideo = {
+      'id': 'Zs6udGtMLS0',
+      'quality': 'default'
+    };
+    var onPlayerReady = function (event) {
+      event.target.playVideo();
+    };
+
+    var done = false;
+    var onPlayerStateChange = function (event) {
+      if (event.data === YT.PlayerState.PLAYING && !done) {
+        setTimeout(callPlayer(playerElementId, 'stopVideo'), 6000);
+        done = true;
+      }
+    };
+
+    $scope.playerFactory = Player;
+    $scope.player = $scope.playerFactory().getPlayer(
+      {
+        'target': playerElementId,
+        'events':{
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      }
+    );
+
+    $scope.pause = function() {
+      callPlayer(playerElementId, 'pauseVideo');
+    };
+
+    $scope.loadVideoById = function() {
+      callPlayer(playerElementId, 'loadVideoById', [$scope.loadVideo.id, 0, $scope.loadVideo.quality]);
+    };
   }]);
