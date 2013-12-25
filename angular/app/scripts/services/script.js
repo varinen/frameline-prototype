2ed062773d241072940d123a4f1de549a075f177
Oleg Ishenko <oleg.ishenko@solvingmagento.com>
2013-12-25 17:02:52 +0100

    First working player algorithm

--- angular/app/scripts/services/script.js
+++ angular/app/scripts/services/script.js
@@ -16,7 +16,11 @@ scriptService.service('script', [
       controls,
       currentPosition,
       offset,
-      time;
+      time,
+      playingCommands,
+      isPlaying,
+      statusData,
+      script = this;
 
     return {
       /**
@@ -75,37 +79,110 @@ scriptService.service('script', [
         return commands;
       },
 
+      /**
+       * Plays the script commands sequence into a player located under the playerElementId element
+       * 
+       * @param playerElementId
+       */
       play: function (playerElementId) {
-        time = new Date().getTime();
-        var k;
-        currentPosition = 0;
-        offset          = 1;
-        controls = controlsFactory().getControls(callPlayer, playerElementId);
-        var callback = this.executeCommand;
-        for (k = 0; k < commands.length; k += 1) {
-          /**
-           * @see http://stackoverflow.com/questions/6425062/passing-functions-to-settimeout-in-a-loop-always-the-last-value
-           */
+        var k, errorMessage, now, statusData;
+        
+        //set starting time
+        time      = new Date().getTime();
+        //it's not playing yet
+        isPlaying = false;
+        controls  = controlsFactory().getControls(callPlayer, playerElementId);
+
+        //clone the commands array
+        playingCommands = commands.slice(0);
+        //start script - must be the first command in the sequence
+        var startScript = playingCommands.shift();
+        if ('startScript' !== Object.keys(startScript.command).shift()) {
+          errorMessage = 'Expected a "startScript" command in the beginning, instead, '
+            + Object.keys(startScript.command).shift() + ' is detected';
+          console.log(errorMessage);
+          throw (errorMessage);
+        }
+
+        //put a script reference into the closure var script
+        script = this;
+        
+        //load video using the startScript command
+        controls.startScript(startScript.command.startScript.args);
+        
+        //start lisening for player messages to schedule the first command when the video is loaded
+        callPlayer(playerElementId, 'listening');
+        window.addEventListener(
+          'message',
+          this.startScheduling,
+          true
+        );
+      },
+
+      /**
+       * Check if the video is started loading and schedulte the first command
+       * 
+       * @param e event to listen to
+       */
+      startScheduling: function(e) {
+        var i, property, data = JSON.parse(e.data);
+        if (
+          ((e.origin === 'http://www.youtube.com') || (e.origin === 'https://www.youtube.com')) &&
+            (data.event === 'infoDelivery')) {
+          //update the private script var statusData 
+          statusData = data.info;
+          // start scheduling if the script is not playing but the video has started loading
+          if (!isPlaying && data.info && data.info.videoBytesLoaded && data.info.videoBytesLoaded > 0) {
+            isPlaying = true;
+            script.scheduleCommand(data.info);
+          }
+        }
+      },
+      /**
+       * Schedule a command
+       *
+       * @param statusData player status data
+       */
+      scheduleCommand: function (statusData) {
+        var command = playingCommands.shift();
+        if (!command || !command.offset || !command.command) {
+          return;
+        }
+        var callback = this.executeCommand,
+          schedule   = parseFloat(command.offset),
+          now        = new Date().getTime();
+
+        console.log((now - time) / 1000 + ': scheduling '+ Object.keys(command.command).shift() + ' ' +
+         'schedule: ' + schedule +
+          ', current: ' + statusData.currentTime + ', offset: ' + command.offset);
+        /**
+         * @see http://stackoverflow.com/questions/6425062/passing-functions-to-settimeout-in-a-loop-always-the-last-value
+         */
           setTimeout(
             (function(commandData) {
               return function() {callback(commandData);};
-            })(commands[k].command),
-            (offset + parseFloat(commands[k].offset)) * 1000
+            })(command.command),
+            (schedule) * 1000
           );
-          currentPosition += (parseFloat(commands[k].offset) + offset);
-        }
+
       },
 
+      /**
+       * Executes a command and schedules the next one in the sequence
+       *
+       * @param command
+       */
       executeCommand: function(command) {
-        var now = new Date().getTime();
-        var commandName = Object.keys(command).shift();
-        console.log((now - time) + ' '+ commandName + ' ' + JSON.stringify(command[commandName].args));
-        //return;
+        var now = new Date().getTime(),
+          commandName = Object.keys(command).shift();
+        console.log('executing ' + ((now - time) / 1000) + ' '+ commandName + ' ' + JSON.stringify(command[commandName].args));
+
         if (commandName === 'setAnchor') {
           offset = currentPosition;
         } else {
           controls[commandName](command[commandName].args);
         }
+        script.scheduleCommand(statusData);
       }
     };
   }
