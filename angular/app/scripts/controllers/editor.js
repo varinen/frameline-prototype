5b3eff45c76f669a618bcae41a438ead78270bc9
Oleg Ishenko <oleg.ishenko@solvingmagento.com>
2014-01-02 17:07:14 +0400

    Further development of the directives

--- angular/app/scripts/controllers/editor.js
+++ angular/app/scripts/controllers/editor.js
@@ -14,10 +14,36 @@ angular.module('angularApp').controller(
     function ($scope, $compile, Player, PlayerControls, commandsDictionary, script, ScriptList) {
 
       var commandNames = Object.keys(commandsDictionary.commands),
-        playerElementId = 'player',i;
+        playerElementId = 'player', i;
+
+      /**
+       * @todo refactor to a service
+       */
+      var getKeyByValue = function(object, value ) {
+        for( var prop in object ) {
+          if( object.hasOwnProperty( prop ) ) {
+            if( object[prop] === value ) {
+              return prop;
+            }
+          }
+        }
+      };
+
+      $scope.setDropDownValue = function(valueToSet, propertyName, commandObject) {
+        var value, commandName = Object.keys(commandObject.command).shift();
+        switch (propertyName) {
+          case 'quality': value = getKeyByValue(commandsDictionary.qualities, valueToSet);
+            break;
+          case 'dimension': value = getKeyByValue(commandsDictionary.dimensions, valueToSet);
+        }
+        commandObject.command[commandName].args[propertyName] = value
+        //$scope.$apply();
+        var b = 1;
+      }
 
       //generate a list of the stored scripts
       $scope.storedScripts = ScriptList.query({action:'list_scripts'});
+      $scope.commandsDictionary = commandsDictionary;
 
       //generate and display the command menu
       $scope.commandsMenu = [];
@@ -39,6 +65,7 @@ angular.module('angularApp').controller(
 
 
       $scope.editScript = function(fileName) {
+        angular.element('div#editor').html('');
         var i, scriptJson = ScriptList.query(
           {action:'get_script', filename:fileName},
           function () {
@@ -62,13 +89,25 @@ angular.module('angularApp').controller(
             var commandKeys = Object.keys($scope.commandList);
             for (i = 0; i < commandKeys.length; i += 1) {
               var commandName = Object.keys($scope.commandList[commandKeys[i]].command).shift();
+
+              //not the brightest idea, but, for every command in the list a property is created for the
+              //scope using the command's name and its id (position) in the command list.
+              //This way it is possible to pass the property name in the attribute to the directive
+              //that will represent it. The assignment is dynamic and the changes made in the directive's
+              //inputs will change the command's properties in the scope.
               $scope['commandObject_' + commandKeys[i]] = $scope.commandList[commandKeys[i]];
+              $scope['commandObject_' + commandKeys[i]].commandKey = commandKeys[i];
               var commandElement =
                 angular.element(document.createElement(commandName.toLowerCase()))
-                  .attr('commandobject', 'commandObject_' + commandKeys[i]);
-              var el = $compile( commandElement)( $scope, function (e1, $scope) { angular.element('div#editor').append(e1)} );
-              var e = 0;
-              //angular.element('div#editor').append(commandElement);
+                  .attr('commandobject', 'commandObject_' + commandKeys[i])
+                  .attr('commandsdictionary', 'commandsDictionary')
+                  .attr('setdropdownvalue', 'setDropDownValue');
+              //the created directive's element is compiled into a dom object and appended to the editor
+              var el = $compile( commandElement)(
+                $scope,
+                function (e1, $scope) { angular.element('div#editor').append(e1)}
+              );
+
             }
 
           },
--- angular/app/scripts/directives/editorcommands.js
+++ angular/app/scripts/directives/editorcommands.js
@@ -4,22 +4,23 @@ angular.module('editorCommands', []);
 angular.module('editorCommands')
   .directive('startscript', function () {
     return {
+      scope: {commandObject: '=commandobject', commandsDictionary: '=commandsdictionary', setDropDownValue: '=setdropdownvalue'},
       templateUrl: '/views/directives/start_script.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
+        var b  = 1;
         //element.text('this is a chart');
       }
-    };
+    }
   });
 
 angular.module('editorCommands')
   .directive('setvolume', function () {
     return {
-      scope: {commandObject: '=commandobject'},
+      scope: {commandObject: '=commandobject', commandsDictionary: '=commandsdictionary'},
       templateUrl: '/views/directives/set_volume.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.currentcommandid = attrs.currentcommandid;
         var b  = 1;
         //element.text('this is a chart');
       }
